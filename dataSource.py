4cdbb16c7ababd5d54494cfe4a19f8bcd915eb70
armenllanos <98763967+armenllanos@users.noreply.github.com>
2023-02-02 15:20:05 -0500

    defining the dataSource class

---
+++ dataSource.py
@@ -0,0 +1,9 @@
+
+
+class DataSource:
+    numberOfWords = 0
+    wordList = dict()##this is going to be a dicctionary just contianing the words
+    def __init__(self, mandatoryLetter, optionalLetters):
+        print("to be done")
+    #hacer la query de  la base de datos 
+    #
