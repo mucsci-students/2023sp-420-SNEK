ff46992c07a45cc955f4b11ad7d12fb468157769
Josue <joperezc@millersville.edu>
2023-02-02 19:04:26 -0500

    This is the inital state class file, only has class variables so that when one file refers to it or changes them they change across all files

---
+++ state.py
@@ -0,0 +1,15 @@
+# simple state class to start
+class state:
+
+    #class variables 
+    puzzle = []
+    wordList = []
+    foundWords = []
+    status = ""
+    points = 0
+
+    #empty init so class is persistent across files
+    def _init_(self):
+        pass
+
+
