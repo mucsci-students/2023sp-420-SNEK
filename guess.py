0c342ce063b4a30200b6da27d901b6ef1a4b1bc8
SClugston9 <stclugst@millersville.edu>
2023-02-02 23:40:43 -0500

    update to guess

--- guess.py
+++ guess.py
@@ -1,14 +1,29 @@
 # Stephen Clugston
 # Guess Class to handle the user making a new guess
 
+from state import state
+
 def guess(userGuess, gameState):
     
-    if gameState.array.contains(userGuess) == True:
-        # points correctly incremented to gameState
-        # remove the word from the words still to guess in gameState, and add it to already guessed words
-    
-    if gameState.array.contains(userGuess) == False:
-        # Output error saying the guess was incorrect
-    
+    if userGuess >= 4:
+
+        if gameState.wordList.contains(userGuess) == True:
+            
+            if gameState.number == 1:
+                # Points correctly incremented to gameState
+                # Message displays saying that the game is complete
+                print("hi") 
         
+            # points correctly incremented to gameState
+            # remove the word from the words still to guess in gameState, and add it to already guessed words
+            print("hi")
+    
+    
+        if gameState.wordList.contains(userGuess) == False:
+            # Output error saying the guess was incorrect
+            print("Guess was not correct")
+    
+    print("Word must be 4 letters or more")
+    
+
 
