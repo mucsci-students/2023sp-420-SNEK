571d92be99b533ea006f6c84f161724736c427e1
SClugston9 <stclugst@millersville.edu>
2023-02-03 00:22:09 -0500

    update to guess

--- guess.py
+++ guess.py
@@ -1,29 +1,37 @@
 # Stephen Clugston
 # Guess Class to handle the user making a new guess
 
-from state import state
+from puzzle import puzzle
 
-def guess(userGuess, gameState):
+def guess(userGuess):
     
     if userGuess >= 4:
-
-        if gameState.wordList.contains(userGuess) == True:
+        if userGuess.contains(puzzle.wordPuzzle[0]):
             
-            if gameState.number == 1:
-                # Points correctly incremented to gameState
-                # Message displays saying that the game is complete
-                print("hi") 
+            if puzzle.wordsList.contains(userGuess):
+            
+                if puzzle.wordListSize == 1:
+                    # Points correctly incremented to gameState, and message displays saying that the game is complete
+                    puzzle.points += 2
+                    --puzzle.wordListSize
+                    print("Congradulations! All words have been guessed!") 
+                    return 1
+
+                # Increments points, removes the word from the possible guesses, and adds the word to found words
+                puzzle.points += 2
+                --puzzle.wordListSize
+                puzzle.wordsList.remove(userGuess)
+                puzzle.foundWords.add(userGuess)
+            
+
+            if not puzzle.wordsList.contains(userGuess):
+                # Output error saying the guess was incorrect
+                print("Guessed word was not correct, try again.")
         
-            # points correctly incremented to gameState
-            # remove the word from the words still to guess in gameState, and add it to already guessed words
-            print("hi")
-    
-    
-        if gameState.wordList.contains(userGuess) == False:
-            # Output error saying the guess was incorrect
-            print("Guess was not correct")
-    
-    print("Word must be 4 letters or more")
-    
+        # The guess did not contain the required letter
+        print("Guessed word must contain the required letter")
+
+    # The guess was less than 4 letters
+    print("Guessed word must be 4 letters or more")
 
 
