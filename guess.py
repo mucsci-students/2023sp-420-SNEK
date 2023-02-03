0c552d42e62c330969d4609854b188531a7478b7
SClugston9 <stclugst@millersville.edu>
2023-02-02 22:17:46 -0500

    start of guess

---
+++ guess.py
@@ -0,0 +1,14 @@
+# Stephen Clugston
+# Guess Class to handle the user making a new guess
+
+def guess(userGuess, gameState):
+    
+    if gameState.array.contains(userGuess) == True:
+        # points correctly incremented to gameState
+        # remove the word from the words still to guess in gameState, and add it to already guessed words
+    
+    if gameState.array.contains(userGuess) == False:
+        # Output error saying the guess was incorrect
+    
+        
+
