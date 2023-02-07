1e4f9fe5126e1fb25937db042f88d447209e4600
armenllanos <98763967+armenllanos@users.noreply.github.com>
2023-02-07 13:31:42 -0500

    query fixed, word selection correct

--- dataSource.py
+++ dataSource.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sqlite3
+import pandas as pd
 
 class DataSource:
     numberOfLetters = 0
@@ -17,19 +18,31 @@ class DataSource:
 
         treatmentMat = np.array(output)
         letterSet = set(treatmentMat[:,0])
-        for letter in optionalLetters:
-            cur.execute("SELECT word FROM word_list WHERE word like '%"+letter+"%'")
-            output = cur.fetchall()
-            con.commit()
-            treatmentMat = np.array(output) 
-            auxSet = set(treatmentMat[:,0])
-            letterSet = letterSet.union(auxSet)
+        
+        treatmentMat = np.array(output)
+        letterSet = set(treatmentMat[:,0])
+        boolList = []
+        for word in letterSet:
+            approved = True
+            for letter in word:
+                if(letter not in optionalLetters):
+                    approved = False
+            boolList.append(approved)
+        wordDF = pd.DataFrame(letterSet)
+        wordDF = wordDF[boolList]
+
+        # for letter in optionalLetters:
+        #     cur.execute("SELECT word FROM word_list WHERE word like '%"+letter+"%'")
+        #     output = cur.fetchall()
+        #     con.commit()
+        #     treatmentMat = np.array(output) 
+        #     auxSet = set(treatmentMat[:,0])
+        #     letterSet = letterSet.union(auxSet)
         con.close()
         self.wordList = letterSet
         numberLettersList = [len(set(list(word))) for word in self.wordList]
         self.numberOfLetters = sum(numberLettersList)
-    def __init__():
-        print("to be done")
+    
 
     def checkWord(searchedWord):
         con = sqlite3.connect("example2.db")
