5a79559e14d6af4861918bf090e13ed6b35bb527
Stephen Clugston <stclugst@millersville.edu>
2023-03-05 20:27:10 -0500

    Added docdumentation to GameController.py

--- src/controller/GameController.py
+++ src/controller/GameController.py
@@ -22,18 +22,24 @@ class GameController:
     __STILL_LOAD_MSG = "Do you want to load another game?"
     __NO_GAME_TITLE = "Not Currently in game:"
 
+    # Private function that is an error message tempalte for actions that aren't allowed when a game is not being played.
     def __NO_GAME_DESC(self, description):
         return f"You can't {description} a game if you are not playing one."
 
+    # Constructor that instanciates a new GameController object.
     def __init__(self, dataSource: DataSource) -> None:
         self.myPuzzle: Puzzle = None
         self.myUserInterface = None
         self.playing = False
         self.myDataSource: DataSource = dataSource
 
+    # Sets the GameController myUserInterface attribute to the the passes UserInterface.
     def setUserInterface(self, myUserInterface: UserInterface.UserInterface):
         self.myUserInterface = myUserInterface
 
+    # Function to proccess input from the user. 
+    # Correctly handles the cases in which a command is called, for when a game is playing and not. 
+    # If the input is not a command, the string is passed to processGuess and evaluated.
     def processInput(self, userInput: str) -> None:
         if Commands.isCommand(userInput):
             self.processCommand(userInput)
@@ -43,6 +49,7 @@ class GameController:
         else:
             self.processGuess(userInput)
 
+    # A private function that asks whether the user wants to save when the program is in the process of exiting.
     def __askExitAndSave(self) -> bool:
         exitGame = self.myUserInterface.getConfirmation(self.__EXIT_MSG)
         if exitGame:
@@ -52,6 +59,8 @@ class GameController:
                 self.__saveFile()
         return exitGame
 
+    # A private function that handles the functionality of saving a file and all of its cases.
+    # Uses the SaveAndLoad module to handle saving a game into the json format.
     def __saveFile(self) -> None:
         overwrite = True
         scratchMode = self.myUserInterface.getConfirmation(
@@ -68,6 +77,9 @@ class GameController:
             else:
                 SaveAndLoad.saveCurrent(self.myPuzzle, fileName)
 
+    # Private function to create a new game from a newBaseWord 
+    # Sets the puzzle attributes accordingly, sets the GameController to playing,
+    # and tells the UI to display the puzzle
     def __createGame(self, newBaseWord):
         newPuzzleLetters = list(set(list(newBaseWord)))
         random.shuffle(newPuzzleLetters)
@@ -76,6 +88,8 @@ class GameController:
         self.playing = True
         self.myUserInterface.showPuzzle(self.myPuzzle)
 
+    # Function to processs the command from a user. processCommand is called from processUserInput.
+    # Handles all commands, such as exit, help, laod, save, rank, guessed words, shuffle, new rnd, new wrd, and show status
     def processCommand(self, commandStr: str) -> None:
         command = Commands.getCommandFromName(commandStr)
         if command == Commands.EXIT:
@@ -171,6 +185,10 @@ class GameController:
             self.myUserInterface.showError(
                 "Not a valid command:", 'Type "!help" to show all possibilities')
 
+    # Function to processs the guess from the user. Called by proccesInput.
+    # Correctly handles the situations in which the guess is correct, but also when the guess is simply incorrect 
+    # (not a word, or not in the database), and more specifically when the guesss isn't longer than 3 letters, 
+    # if the guess doesn't have the required letter, and the word was already guessed.
     def processGuess(self, userGuess: str):
         if len(userGuess) < Puzzle.MIN_WRD_LEN:
             self.myUserInterface.showWrongGuess(
@@ -199,3 +217,4 @@ class GameController:
         if currentPoints == maxPoints:
             self.myUserInterface.showEnd()
             self.playing = False
+           
