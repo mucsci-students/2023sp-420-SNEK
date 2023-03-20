ffaa63efca7d701e5172898a43143dc7815b8e53
armenllanos <98763967+armenllanos@users.noreply.github.com>
2023-03-20 16:36:48 -0400

    static fixed

--- src/model/Hint.py
+++ src/model/Hint.py
@@ -1,10 +1,5 @@
 class Hint:
-    letterMatrix = dict()
-    beginingList = dict()  
-    pangram = 0
-    perfectPangram = 0
-    bingo =  False
-    numberOfWords = 0
+ 
     def __init__(self, letterMatrix:dict[str, dict[str, int]], beginingList:dict[str,int], pangram:int , perfectPangram:int , bingo:bool, numberOfWords:int ):
         self.beginingList = beginingList
         self.letterMatrix = letterMatrix
