78137e5107a6c92ac712c4be3cf0de6e60df7bc8
armenllanos <98763967+armenllanos@users.noreply.github.com>
2023-03-20 16:19:40 -0400

    number of words attribute added

--- src/model/Hint.py
+++ src/model/Hint.py
@@ -4,9 +4,11 @@ class Hint:
     pangram = 0
     perfectPangram = 0
     bingo =  False
-    def __init__(self, letterMatrix:dict[str, dict[str, int]], beginingList:dict[str,int], pangram:int , perfectPangram:int , bingo:bool ):
+    numberOfWords = 0
+    def __init__(self, letterMatrix:dict[str, dict[str, int]], beginingList:dict[str,int], pangram:int , perfectPangram:int , bingo:bool, numberOfWords:int ):
         self.beginingList = beginingList
         self.letterMatrix = letterMatrix
         self.pangram = pangram
         self.perfectPangram = perfectPangram
         self.bingo = bingo
+        self.numberOfWords = numberOfWords
