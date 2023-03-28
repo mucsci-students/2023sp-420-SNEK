724549885d4cf2dca4169211068ca8b03b3629b4
armenllanos <98763967+armenllanos@users.noreply.github.com>
2023-03-28 11:22:15 -0400

    Update DataSource.py

--- src/model/DataSource.py
+++ src/model/DataSource.py
@@ -61,37 +61,33 @@ class DataSource:
         self.numberOfLetters = sum(numberLettersList)
         self.wordList = list(self.wordList[0])
 
-    # returns a  dataSource object built with the word and the mandatory letter
-    def grabWordsFor(self, word, mandatoryLetter):
-        optionalLetters = list(set(word))
-        if mandatoryLetter == None or optionalLetters == None:
-            return
+  
+    def getRandomWord(self):
         con = sqlite3.connect(self.dbName)
         cur = con.cursor()
-        if len(mandatoryLetter) > 1:  # to avoid the user makes an injection
-            return
+
         cur.execute(
-            "SELECT word FROM word_list WHERE word like '%"+mandatoryLetter+"%'")
+            "SELECT word,differentLetters FROM word_list WHERE numLetter = 7 or numLetter > 7")
         output = cur.fetchall()
-        con.commit()
         treatmentMat = np.array(output)
-        wordSet = set(treatmentMat[:, 0])
-        # just keep the words that contain only the desired letters
+        wordDF = (treatmentMat[:, 1])
+        depWordDF = pd.DataFrame(treatmentMat[:, 0])
+
         boolList = []
-        for word in wordSet:
+        for word in wordDF:
             approved = True
-            for letter in word:
-                if (letter not in optionalLetters):
-                    approved = False
+            if (len(word) < 7):
+                approved = False
             boolList.append(approved)
-        wordDF = pd.DataFrame(wordSet)
-        wordDF = wordDF[boolList]
+
+        depWordDF = depWordDF[boolList]
+        con.commit()
+        numero = random.randint(0, len(depWordDF)-1)
+
+        auxList = list(depWordDF.iloc[numero])
+
         con.close()
-        self.wordList = wordDF
-        numberLettersList = [len(set(list(word)))
-                             for word in list(self.wordList[0])]
-        self.numberOfLetters = sum(numberLettersList)
-        self.wordList = list(self.wordList[0])
+        return auxList[0]
 
     def getHints(self, wordList:list,optionalLetters:list)->Hint:
         letterMat = dict()
